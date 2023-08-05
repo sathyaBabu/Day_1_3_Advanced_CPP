b0b0c86e80a305ff8ff89505dc7476b4c3e7b4df
Sathya P <sathyasoft123@gmail.com>
2023-08-05 07:28:30 +0530

    VISITOR commited

---
+++ gitAppVariousStages/gitAppVariousStages/ReadMe.md
@@ -0,0 +1,12 @@
+//
+//  ReadMe.md
+//  gitAppVariousStages
+//
+//  Created by Sathya Babu on 04/08/23.
+//
+
+#ifndef ReadMe_h
+#define ReadMe_h
+
+
+#endif /* ReadMe_h */
---
+++ gitAppVariousStages/gitAppVariousStages/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  gitAppVariousStages
+//
+//  Created by Sathya Babu on 04/08/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
